7b841cd16af2f0f6c41c9ca56a3ea54ea90dc244
loganpepple <110237411+loganpepple@users.noreply.github.com>
2022-11-16 14:32:49 -0500

    dynamically update exercises

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,5 +1,4 @@
-{
-    "charts": [
+const exerciseData = [
         {
             "exercises": [
                 {
@@ -7,29 +6,29 @@
                     "description": "Feet astride, arms upward. Forward bend to floor touching then stretch upward and backward bend. Don not strain to keep knees straight."
                 },
                 {
-                    "reps": [],
-                    "description": ""
+                    "reps": [3,4,5,7,8,9,11,12,13,15,17,18],
+                    "description": "Back lying, feet 6\" apart, arms at sides. Sit up just far enough to see your heels. Keep legs straight, head and shoulders must clear the floor."
                 },
                 {
-                    "reps": [],
-                    "description": ""
+                    "reps": [4,5,6,8,10,12,13,14,15,16,17,18],
+                    "description": "Front lying, plams placed under the thighs. Raise head and one leg, repeat using legs alternately. Keep leg straight at the knee, thighs must clear the palms. Count one each time second leg touches floor."
                 },
                 {
-                    "reps": [],
-                    "description": ""
+                    "reps": [2,3,3,4,5,6,7,8,9,11,12,13],
+                    "description": "Front lying, hands unde rthe shoulders, palms flat on the floor. Straighted arms lifting upper body, keeping he knees on the floor. Bend arms to lower body. Keep body straight from the knees, arms must be fully extended, chest must touch floor to complete one movement."
                 },
                 {
-                    "reps": [],
-                    "description": ""
+                    "reps": [100,145,175,205,235,260,280,305,320,335,375,400],
+                    "description": "Stationary run - (count a step each time left foot touches floor - Lift feet approximately 4 inches off floor.) Every 75 steps do 10 \"scissor jumps\". Repeat this sequence until required number of steps is completed.\nScissor jumps - Stand with right leg and left arm extended forward, and left leg and right arm extended backward. Jump up - change position of arms and legs before landing. Repeat (arms shoulder high)."
                 }
             ],
             "run": {
                 "distance": "1/2 mile",
-                "time": [8, 7.5, 7, 6.5, 6.5, 6.5, 6, 6, 6, 5.5, 5.5, 5.5]
+                "time": [8,7.5,7,6.5,6.5,6.5,6,6,6,5.5,5.5,5.5]
             },
             "walk": {
                 "distance": "1 mile",
-                "time": [21, 21, 20, 19, 19, 19, 18, 18, 18, 17, 17, 17]
+                "time": [21,21,20,19,19,19,18,18,18,17,17,17]
             }
         },
         {
@@ -161,4 +160,3 @@
             }
         }
     ]
-}
--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,45 @@ $(document).ready(function() {
 
 function appStart() {
     $('body').load('../views/levelSelect.html', function() {
+        $('#chart-number').text(chartList[selectedChart]);
+        $('#level-number').text(levelList[selectedLevel]);
+
         $('.change-difficulty').click(function() {
             changeDifficulty(this);
+            //todo: animate selection
         })
 
         $('#next').click(function() {
             fadeOut($('#difficulty-select'));
-            setTimeout(function() {appStart()}, 700);
+            setTimeout(function() {loadPreview()}, 700);
         })
     });
     
 }
 
 function loadPreview() {
-    $('body').load('..views/workoutPreview.html', function() {
+    $('body').load('../views/workoutPreview.html', function() {
+        // dynamically change exercise info based on selections
         const exerciseNodes = $('.exercise');
-        exerciseNodes.forEach((exerciseNode, exerciseNumber) => {
+        let {exercises, run, walk} = exerciseData[selectedChart];
+        [...exerciseNodes].forEach((exerciseNode, exerciseNumber) => {
+            $(exerciseNode).find('h3').text(exercises[exerciseNumber].reps[selectedLevel]);
+            $(exerciseNode).find('.explanation').text(exercises[exerciseNumber].description);
+            // todo: create workout images
+        })
+        $('#run-div > h5').text(`${run.distance} run in`);
+        $('#run-div > h3').text(`${run.time[selectedLevel]} minutes`);
+        $('#walk-div > h5').text(`${walk.distance} walk in`);
+        $('#walk-div > h3').text(`${walk.time[selectedLevel]} minutes`);
 
+        $('input').click(function() {
+            finalExercise = this.id;
+            //todo: animate selection
+        })
+
+        $('#cancel').click(function() {
+            fadeOut($('#workout-preview'));
+            setTimeout(function() {appStart()}, 700);
         })
 
         $('#start').click(function() {
@@ -32,7 +54,7 @@ function loadPreview() {
 }
 
 function loadWorkout() {
-
+    $('body').load()
 }
 
 
@@ -40,28 +62,28 @@ function changeDifficulty ({parentNode, classList}) {
     const increment = ([...classList].includes('up')) ? 1 : -1;
     const setting = (parentNode.id == 'chart-select') ? 'chart' : 'level';
     const text = $(parentNode).find('p');
-    console.log(text);
-
 
     if (setting == 'chart') {
-        if (chart > 0 && chart < 5 || (chart == 0 && increment > 0) || (chart == 5 && increment < 0)) {
-            chart += increment;
+        if (selectedChart > 0 && selectedChart < 5 || (selectedChart == 0 && increment > 0) || (selectedChart == 5 && increment < 0)) {
+            selectedChart += increment;
         }
-        $(text).text(charts[chart]);
+        $(text).text(chartList[selectedChart]);
     } else {
-        if (level > 0 && level < 11 || (level == 0 && increment > 0) || (level == 11 && increment < 0)) {
-            level += increment;
+        if (selectedLevel > 0 && selectedLevel < 11 || (selectedLevel == 0 && increment > 0) || (selectedLevel == 11 && increment < 0)) {
+            selectedLevel += increment;
         }
-        $(text).text(levels[level]);
+        $(text).text(levelList[selectedLevel]);
     }
 }
 
 function fadeOut(node) {
+    console.log('fading');
     $(node).removeClass('fade-in').addClass('fade-out');
     setTimeout(function() {$(node).remove()}, 700);
 }
 
-const charts = [1,2,3,4,5];
-const levels = ['D-','D','D+','C-','C','C+','B-','B','B+','A-','A','A+'];
-var chart = 0;
-var level = 0;
+const chartList = [1,2,3,4,5];
+const levelList = ['D-','D','D+','C-','C','C+','B-','B','B+','A-','A','A+'];
+var selectedChart = 0;
+var selectedLevel = 0;
+var finalExercise = 'exercise-five';
